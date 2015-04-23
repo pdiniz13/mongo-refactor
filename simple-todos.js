00687891283dda6ad6093149efce7d52dcae63f9
Paulo Diniz <pdiniz_100@yahoo.com>
2015-04-23 11:12:32 -0700

    Very simple todos in SQL after refactor

--- simple-todos.js
+++ simple-todos.js
@@ -2,8 +2,7 @@ tasks = new SQL.Collection('tasks', 'postgres://postgres:1234@localhost/postgres
 
 if (Meteor.isClient) {
 
-  var taskTable = { id: ['$number'], text: ['$string', '$notnull'], checked: ['$bool'], createdat: ['$date'] };
-  tasks.createTable(taskTable);
+  tasks.createTable({ id: ['$number'], text: ['$string', '$notnull'], checked: ['$bool'], createdat: ['$date'] });
 
   Template.body.helpers({
     tasks: function () {
@@ -31,6 +30,7 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
 
   //tasks.createTable({text: ['$string'], checked: ["$bool", {$default: false}]}).save();
+
   tasks.publish('tasks', function(){
     return tasks.select('id', 'text', 'checked', 'createdat').order('createdat DESC');
   });
