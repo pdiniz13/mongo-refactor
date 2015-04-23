47f932f85b736b1c3d5b0130094a9096ab668744
Paulo Diniz <pdiniz_100@yahoo.com>
2015-04-23 10:17:49 -0700

    Very simple todos in mongo

--- simple-todos.js
+++ simple-todos.js
@@ -1,61 +1,34 @@
 
 tasks = new Mongo.Collection('tasks');
-username = new Mongo.Collection('username');
 
 if (Meteor.isClient) {
 
   Meteor.subscribe('tasks');
-  Meteor.subscribe('username');
-
-  Session.set('user', 'all')
-
 
   Template.body.helpers({
-    usernames: function () {
-      return username.find({});
-    },
     tasks: function () {
-      if (Session.get('user') === 'all'){
         return tasks.find({});
       }
-      return tasks.find({name: Session.get('user')});
-    }
   });
 
   Template.body.events({
     "submit .new-task": function (event) {
-      if (event.target.category.value){
         var text = event.target.text.value;
-        var user = event.target.category.value;
         tasks.insert({
-          text: text;,
-          checked:false,
-          name: user
+          text: text,
+          checked:false
         });
         event.target.text.value = "";
-      } else{
-        alert("please add a user first");
-      }
       return false;
     },
-    "submit .new-user": function (event) {
-      var text = event.target.text.value;
-      username.insert({
-        name:text
-      });
-      event.target.text.value = "";
 
-      return false;
-    },
     "click .toggle-checked": function () {
       tasks.update(this._id, {$set: {checked: ! this.checked}});
     },
     "click .delete": function () {
       tasks.remove(this._id);
-    },
-    "change .catselect": function(event){
-      Session.set('user',event.target.value);
     }
+
   });
 
 }
@@ -66,7 +39,4 @@ if (Meteor.isServer) {
     return tasks.find({});
   });
 
-  Meteor.publish('username', function(){
-    return username.find({});
-  });
 }
